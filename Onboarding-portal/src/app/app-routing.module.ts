bbb70f1abafa12e7ce3dfce175faecffb2e7a70f
Rujul Walvekar <rujul.w@gmail.com>
2021-05-08 03:34:36 +0530

    App-Updates

--- Onboarding-portal/src/app/app-routing.module.ts
+++ Onboarding-portal/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
+import { componentFactoryName } from '@angular/compiler';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DisplayAdminComponent } from './components/displayAdmin/displayAdmin.component';
+import { DisplayOnboardeeComponent } from './components/displayOnboardee/displayOnboardee.component';
+import { AddOnboardeeComponent} from './components/addOnboardee/addOnboardee.component';
+import { AddAdminComponent } from './components/addAdmin/addAdmin.component';
+import { UpdateOnboardeeComponent } from './components/updateOnboardee/updateOnboardee.component';
+import { UpdateAdminComponent } from './components/updateAdmin/updateAdmin.component';
+// import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/displayOnboardee', pathMatch: 'full' },
+  // { path: 'login', component: LoginComponent },
+  { path: 'displayOnboardee' , component:DisplayOnboardeeComponent},
+  { path: 'displayAdmin' , component:DisplayAdminComponent},
+  { path: 'addOnboardee' ,component:AddOnboardeeComponent},
+  { path: 'addAdmin' ,component:AddAdminComponent},
+  { path: 'updateOnboardee/:emailID', component:UpdateOnboardeeComponent},
+  { path: 'updateAdmin/:aemail', component:UpdateAdminComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Onboarding-portal/src/app/app.module.ts
+++ Onboarding-portal/src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayOnboardeeComponent} from './components/displayOnboardee/displayOnboardee.component';
+import { RouterModule} from "@angular/router";
+import { FormsModule} from "@angular/forms";
+
+import { HttpClientModule } from '@angular/common/http'
+import { DisplayAdminComponent } from './components/displayAdmin/displayAdmin.component';
+import { AddOnboardeeComponent } from './components/addOnboardee/addOnboardee.component';
+import { AddAdminComponent } from './components/addAdmin/addAdmin.component';
+import { UpdateAdminComponent } from './components/updateAdmin/updateAdmin.component';
+import { UpdateOnboardeeComponent } from './components/updateOnboardee/updateOnboardee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayOnboardeeComponent,
+    DisplayAdminComponent,
+    AddOnboardeeComponent,
+    AddAdminComponent,
+    UpdateOnboardeeComponent,
+    UpdateAdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
